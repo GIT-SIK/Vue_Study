4225a7719b5f5fbacc947c50e8446964b22b9051
GIT-SIK <xeno_s@naver.com>
2025-01-10 22:08:23 +0900

    feat : 공통 컴포넌트, Axios 전역 설정

---
+++ src/components/global/BaseButton.vue
@@ -0,0 +1,64 @@
+<template>
+  <button :class="['base-button']" :disabled="disabled"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.base-button {
+  width: 100px;
+  height: 40px;
+  font-family: 'Lato', sans-serif;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  line-height: 42px;
+  padding: 0;
+  border: none;
+}
+.base-button:hover {
+  background: transparent;
+  color: #000;
+  box-shadow:
+    -7px -7px 20px 0px #fff9,
+    -4px -4px 5px 0px #fff9,
+    7px 7px 20px 0px #0002,
+    4px 4px 5px 0px #0001;
+}
+.base-button:before,
+.base-button:after {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 2px;
+  width: 0;
+  background: #000;
+  transition: 400ms ease all;
+}
+.base-button:after {
+  right: inherit;
+  top: inherit;
+  left: 0;
+  bottom: 0;
+}
+.base-button:hover:before,
+.base-button:hover:after {
+  width: 100%;
+  transition: 800ms ease all;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,36 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import api from '@/utils/api'
 
 const app = createApp(App)
 
-app.use(router)
+/* 공통 Axios 전역 지정 */
+app.config.globalProperties.$api = api
 
-app.mount('#app')
+/* 공통 뷰 전역 지정*/
+const globalComponents = import.meta.glob('@/components/global/*.vue')
+
+const loadComponents = async () => {
+  // async -> await
+  try {
+    for (const [path, loader] of Object.entries(globalComponents)) {
+      const module = await loader()
+      console.log('path: ', path)
+      const componentName = path
+      .split('/')
+      .pop()
+      .replace(/\.\w+$/, '');
+
+      console.log('공통 컴포넌트 등록: ', module.default.name)
+
+      app.component(componentName, module.default)
+    }
+  } catch (error) {
+    console.error('[main.js] Error loading global components:', error)
+  }
+}
+loadComponents().then(() => {
+  app.use(router)
+  app.mount('#app')
+})
