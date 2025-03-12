4d8c4f5786e61c6e11309ff4d7aee6840ff76579
SIK <96171462+GIT-SIK@users.noreply.github.com>
2025-03-12 23:18:17 +0900

    feat : [디자인] Layout2 -  VuetifySubCard1 추가
    * 추가사항
    - SubCard1 : Tab (Align : center, Variant : text)

---
+++ src/components/design/layout2/VuetifySubCard1.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card variant="text" color="brown-darken-2">
+    <v-tabs
+      v-model="tab"
+      bg-color="transparent"
+      color="brown-darken-2"
+      align-tabs="center"
+    >
+      <v-tab
+        v-for="(item,index) in items"
+        :key="index"
+        :text="item.tabName"
+        :value="item"
+        >{{item.tabName}}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-window v-model="tab">
+      <v-tabs-window-item
+        v-for="(item,index) in items"
+        :key="index"
+        :value="item"
+      >
+        <v-card flat>
+          <v-card-text>{{ item.tabText }}</v-card-text>
+          <!-- <component :is="item.component" /> -->
+        </v-card>
+      </v-tabs-window-item>
+    </v-tabs-window>
+  </v-card>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  /* tab 유지 : pinia 처리 */
+  const tab = ref('0')
+
+  const items = [
+    { tabName: 'Tab1', tabText: '텍스트1' },
+    { tabName: 'Tab2', tabText: '텍스트2' },
+    { tabName: 'Tab3', tabText: '텍스트3' },
+    { tabName: 'Tab4', tabText: '텍스트4' },
+  ]
+</script>
