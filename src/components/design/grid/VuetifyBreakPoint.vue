8388a36fae277577c274ea7b8ba8094115bddd04
GIT-SIK <xeno_s@naver.com>
2025-03-16 00:04:04 +0900

    feat (Vuetify): BreakPoint - 사이즈 동적 조정

---
+++ src/components/design/grid/VuetifyBreakPoint.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card :height="height" title="Responsive Card" subtitle="screen size">
+  </v-card>
+</template>
+
+
+<script setup>
+  import { computed, ref, inject } from 'vue'
+  import { useDisplay } from 'vuetify'
+
+  const { name } = useDisplay()
+  const showToast = inject('showToast')
+
+  const height = computed(() => {
+    showToast("DISPLAY SIZE : " + name.value)
+
+    switch (name.value) {
+      case 'xs': return 220
+      case 'sm': return 400
+      case 'md': return 500
+      case 'lg': return 600
+      case 'xl': return 800
+      case 'xxl': return 1200
+    }
+
+    return undefined
+  })
+</script>
+
